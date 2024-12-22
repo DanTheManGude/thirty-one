bc0560f1f815ae4c7404218d361701bdbd5dc65e
DanTheManGude <dgude31@gmail.com>
2024-12-22 15:49:26 -0500

    create a game

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -3,18 +3,39 @@
 import { useAuth } from "@/context/AuthContext";
 import { Button, Stack, Typography } from "@mui/material";
 import { User } from "firebase/auth";
+import { child, getDatabase, push, ref, update } from "firebase/database";
+import { useState } from "react";
+
+const gamePath = "games";
+const gameTemplate = { players: ["Danny G"] };
 
 function NewGameControls({ user }: { user?: User }) {
+  const [gameId, setGameId] = useState<string | null>(null);
+
   if (!user) {
     return null;
   }
 
+  const handleCreateGame = async () => {
+    const key = push(child(ref(getDatabase()), gamePath)).key;
+
+    await update(ref(getDatabase()), {
+      [`${gamePath}/${key}`]: gameTemplate,
+    });
+
+    setGameId(key);
+  };
+
   return (
     <Stack>
-      <Typography>New Game</Typography>
-      <Button>
+      <Typography textAlign={"center"}>New Game</Typography>
+      <Button onClick={handleCreateGame} disabled={Boolean(gameId)}>
         <Typography>Create</Typography>
       </Button>
+      <Button disabled={!gameId}>
+        <Typography textAlign={"center"}>Start game</Typography>
+      </Button>
+      <Typography>{gameId}</Typography>
     </Stack>
   );
 }
